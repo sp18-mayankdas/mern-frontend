a01a38e28f94e6e5e66120035323f2ab7582468a
sp18-mayankdas <mayank.das@sparkeighteen.com>
2025-07-15 13:28:19 +0530

    Part-3: JSX, Components , React elements

--- React/Lecture-02/App.js
+++ React/Lecture-02/App.js
@@ -1,9 +1,47 @@
-import React from "react"
+import React, { useState } from "react"
 import ReactDOM from "react-dom/client"
 
+//React.Create ELement = (object)=>Html Element ==> React element
+const heading = React.createElement("h1", {}, "Hello Part 2");
+console.log(heading);
 
-const root  = ReactDOM.createRoot(document.getElementById("root"))
 
-const heading = React.createElement("h1" , {} , "Hello Part 2");
+//JSX - HTML-like OR XML-like like syntax ==> React element
+const element = (<span>React element</span>)
 
-root.render(heading)
+const jsxHeading = (<h1>Jsx heading  {element}</h1 >)
+console.log(jsxHeading);
+
+
+//React components
+const Body = () => {
+    return (
+        <h2>Hello world from body!</h2>
+    )
+}
+
+const Head = () => {
+    return (
+        <div>
+            <h1>Hello world from head!</h1>
+            <Body />
+            {heading} {/* You can use react element , javascript variable , any javascript function inside this curely braces  , you can put any js code inside this curly braces*/}
+            {jsxHeading}
+            {/* {console.log("HEllo world")} */}
+
+            {Body()}
+            <Body></Body>
+        </div>
+    )
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(<Head />);
+
+//- const jsxHeading = (<h1>Jsx heading</h1 >) ==>  Babel Transpiles it into react.createElement.
+//- Jsx is trasnspiled by the compilers or bundlers under the hood before reaches it it into the document
+// - At the end of the day ,functional components are functions only.
+// - At the end of the day, React elements are converted into react.createElements and react.createElements are nothing but javascript objects.
+// - {}--> Inside these curly braces you can execute any piece of javscript code , you can console,log , you can call function , you can use variables , you can use react elements , you can use javascript variables, you can even call the functional components inside these curly braces coz functional components are also functions. 
+// - {} --> With the help of these curly braces you can even put react elements within other react elements , you can put react element into component , components into react elements , anything.
+// - {}--> Also these curly braces do the data sanitization , so igt will protect your browser from malicious data .
