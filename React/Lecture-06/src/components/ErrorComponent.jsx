c6ee7a170b78345db584b22b6fcfa915dbca0808
sp18-mayankdas <mayank.das@sparkeighteen.com>
2025-07-19 17:54:25 +0530

    Part-7 Routing , useNavigate

--- React/Lecture-06/src/components/ErrorComponent.jsx
+++ React/Lecture-06/src/components/ErrorComponent.jsx
@@ -1,14 +1,21 @@
-import { useRouteError } from "react-router-dom"
+import { useNavigate, useRouteError } from "react-router-dom"
 
 export default function ErrorComponent() {
 
     const error = useRouteError();
+    const navigate = useNavigate();
+
     console.log(error)
     return (
         <div className="error">
             <h1>Oops!!! </h1>
             <h1>Something Went Wrong!!!</h1>
             <h3>{error.status + " " + error.statusText}</h3>
+            <button className="backBtn" onClick={() => {
+                navigate(-1)
+            }}>
+                Go Back
+            </button>
         </div>
     )
 }
