1d36e2f862410b9784964449783ee8dc864630ff
sp18-mayankdas <mayank.das@sparkeighteen.com>
2025-07-22 14:16:19 +0530

    tweaks in login form

--- React/Lecture-07/src/sections/Login.jsx
+++ React/Lecture-07/src/sections/Login.jsx
@@ -6,7 +6,7 @@ function Login() {
 
     const navigate = useNavigate();
 
-    const { isLoggedIn, setIsLoggedIn, login, logout } = useAuth();
+    const { isLoggedIn, login, logout } = useAuth();
 
     function submitHandler(e) {
         e.preventDefault()
